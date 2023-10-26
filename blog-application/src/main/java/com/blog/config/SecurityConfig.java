b507a6e50d742b24d65c80d1c47b8362caebf79b
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2023-10-26 13:41:04 +0530

    security configuration without the WebSecurityConfigurerAdapter

--- blog-application/src/main/java/com/blog/config/SecurityConfig.java
+++ blog-application/src/main/java/com/blog/config/SecurityConfig.java
@@ -4,20 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)// prepost is enabled to enable pre-authorize for role specific access
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true) // prepost is enabled to enable pre-authorize for role specific
+													// access
+public class SecurityConfig {// extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private CustomUserDetailService customUserDetailService;
@@ -28,34 +31,63 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtFilter jwtFilter;
 
-	
-	/*@Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-          .ignoring()
-          .antMatchers(HttpMethod.POST,"/blog/logins/jwt");
-        //completely bypass the Spring Security Filter Chain.
-    }*/
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	/*
+	 * @Override public void configure(WebSecurity web) throws Exception { web
+	 * .ignoring() .antMatchers(HttpMethod.POST,"/blog/logins/jwt"); //completely
+	 * bypass the Spring Security Filter Chain. }
+	 */
+
+	/*
+	 * // This configuration is done when extended the WebSecurityConfigurerAdapter
+	 * Class
+	 * 
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * 
+	 * http // for disable cross site request forgery .csrf().disable()
+	 * 
+	 * //disable cors .cors().disable()
+	 * 
+	 * // to authorized any HTTP request .authorizeHttpRequests()
+	 * 
+	 * // set the URL which will have permit at first and after that every URL will
+	 * be permitted .antMatchers("/logins/jwt").permitAll()
+	 * .antMatchers("/users/post").permitAll()
+	 * 
+	 * // to authenticate user request with basic authentication
+	 * .anyRequest().authenticated() //.and().httpBasic()
+	 * 
+	 * // configure for authentication entry point
+	 * .and().exceptionHandling().authenticationEntryPoint(
+	 * jwtAuthenticationEntryPoint)
+	 * 
+	 * // making session STATELESS
+	 * .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.
+	 * STATELESS);
+	 * 
+	 * http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+	 * 
+	 * }
+	 */
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http
 				// for disable cross site request forgery
 				.csrf().disable()
-				
-				//disable cors
+
+				// disable cors
 				.cors().disable()
 
 				// to authorized any HTTP request
 				.authorizeHttpRequests()
-				
-				// set the URL which will have permit at first and after that every URL will be permitted
-				.antMatchers("/logins/jwt").permitAll()	
-				.antMatchers("/users/post").permitAll()
-				
+
+				// set the URL which will have permit at first and after that every URL will be
+				// permitted
+				.antMatchers("/logins/jwt").permitAll().antMatchers("/users/post").permitAll()
+
 				// to authenticate user request with basic authentication
-				.anyRequest().authenticated() //.and().httpBasic()
+				.anyRequest().authenticated() // .and().httpBasic()
 
 				// configure for authentication entry point
 				.and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
@@ -65,13 +97,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
+		http.authenticationProvider(daoAuthenticationProvider());
+
+		DefaultSecurityFilterChain securityFilterChain = http.build();
+
+		return securityFilterChain;
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * 
+	 * auth.userDetailsService(customUserDetailService).passwordEncoder(
+	 * passwordEncoder());
+	 * 
+	 * }
+	 */
 
-		auth.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
+	@Bean
+	DaoAuthenticationProvider daoAuthenticationProvider() {
 
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+		provider.setUserDetailsService(this.customUserDetailService);
+		provider.setPasswordEncoder(passwordEncoder());
+
+		return provider;
 	}
 
 	@Bean
@@ -80,11 +131,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 
 	}
-	
-	
+
+	/*
+	 * @Bean
+	 * 
+	 * @Override public AuthenticationManager authenticationManagerBean() throws
+	 * Exception { return super.authenticationManagerBean(); }
+	 */
+
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+
+		return configuration.getAuthenticationManager();
+
 	}
 }
